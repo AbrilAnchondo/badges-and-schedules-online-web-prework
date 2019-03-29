3df3286b93fc5fdb2afcf49274d52372f4708f12
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-29 06:17:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_messages =  [ ]
+  array.each do |x|
+    y = badge_maker(x)
+    badge_messages.push(y)
+  end
+  return badge_messages
+end
+
+def assign_rooms(array)
+  room_assign = []
+  array.each_with_index {|person, i| 
+  room_assign.push("Hello, #{person}! You'll be assigned to room #{i + 1}!")}
+   return room_assign
+end
+
+def printer
+   
+    puts batch_badge_creator
+end
